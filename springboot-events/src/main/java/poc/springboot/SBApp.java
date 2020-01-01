6f47a47442e7526ef687e09cbee734868388c26c
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-01-01 16:56:56 +0530

    springboot-events: Initial commit

---
+++ springboot-events/src/main/java/poc/springboot/SBApp.java
@@ -0,0 +1,22 @@
+package poc.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class SBApp implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.debug("run service");
+	}
+
+}
