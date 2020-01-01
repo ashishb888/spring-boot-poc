b05f64e718203f720d2233f10dfa3aef3f4562d6
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-01-01 17:22:38 +0530

    springboot-events: Run 2

--- springboot-events/src/main/java/poc/springboot/SBApp.java
+++ springboot-events/src/main/java/poc/springboot/SBApp.java
@@ -1,17 +1,24 @@
 package poc.springboot;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import lombok.extern.slf4j.Slf4j;
+import poc.springboot.listener.ApplicationContextInitializedEventListener;
+import poc.springboot.listener.ApplicationEnvironmentPreparedEventListener;
+import poc.springboot.listener.ApplicationPreparedEventListener;
+import poc.springboot.listener.ApplicationStartingEventListener;
 
 @SpringBootApplication
 @Slf4j
 public class SBApp implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SBApp.class, args);
+		new SpringApplicationBuilder(SBApp.class)
+				.listeners(new ApplicationStartingEventListener(), new ApplicationEnvironmentPreparedEventListener(),
+						new ApplicationContextInitializedEventListener(), new ApplicationPreparedEventListener())
+				.run(args);
 	}
 
 	@Override
