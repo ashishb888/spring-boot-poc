40bfc1762820fe63d1f2af7aaadb2d08146bad1a
Ashish Bhosle <ashish.bhosle008@gmail.com>
2020-01-02 15:09:31 +0530

    externalized-config: Moved random code to RandomService

--- externalized-config/src/main/java/poc/springboot/service/RandomService.java
+++ externalized-config/src/main/java/poc/springboot/service/RandomService.java
@@ -4,17 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import poc.springboot.properties.AppProperties;
 
 @Service
 @Slf4j
-public class SBService {
+public class RandomService {
 
 	@Autowired
-	private RandomService rs;
+	private AppProperties ap;
+
+	private void start() {
+		log.debug("start service");
+
+		log.debug("random: " + ap.getRandom());
+	}
 
 	public void main() {
 		log.debug("main service");
 
-		rs.main();
+		start();
 	}
 }
