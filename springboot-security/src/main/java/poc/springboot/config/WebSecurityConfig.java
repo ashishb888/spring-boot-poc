0667a8973fe77dc5ae95f4f9495ffb596f29fbc8
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-19 17:53:03 +0530

    springboot-security: Configured authorization

--- springboot-security/src/main/java/poc/springboot/config/WebSecurityConfig.java
+++ springboot-security/src/main/java/poc/springboot/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package poc.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,6 +15,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("ashish").password("nopass").roles("USER").and().withUser("admin")
 				.password("nopass").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN").antMatchers("/user").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/").permitAll().and().formLogin();
 	}
 
 	@Bean
--- springboot-security/src/main/java/poc/springboot/controller/HomeController.java
+++ springboot-security/src/main/java/poc/springboot/controller/HomeController.java
@@ -10,4 +10,14 @@ public class HomeController {
 	public String home() {
 		return "<h1>Welcome home</h1>";
 	}
+
+	@GetMapping("/user")
+	public String user() {
+		return "<h1>Welcome user</h1>";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "<h1>Welcome admin</h1>";
+	}
 }
