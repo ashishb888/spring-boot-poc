553ef6af47fabc661b27551565a339aa83594ec7
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-20 10:32:43 +0530

    sb-security-jdbc: Initial commit

---
+++ sb-security-jdbc/src/main/java/poc/springboot/SbSecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityJdbcApplication.class, args);
+	}
+
+}
