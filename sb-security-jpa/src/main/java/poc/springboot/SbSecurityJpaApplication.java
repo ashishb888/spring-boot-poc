ea2eae4d4b895a22de19fac893820e61fa7d7085
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-20 13:49:29 +0530

    sb-security-jpa: Initial commit

---
+++ sb-security-jpa/src/main/java/poc/springboot/SbSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ sb-security-jpa/src/main/java/poc/springboot/config/UserDetailsServiceImpl.java
@@ -0,0 +1,14 @@
+package poc.springboot.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+}
