93acb6ed527a88e0cf3eb6bf8c2087cafc44aa2c
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-20 18:02:12 +0530

    sb-security-jwt: Initial commit

---
+++ sb-security-jwt/src/test/java/poc/springboot/SbSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
