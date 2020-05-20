540149cc342c4c71938ad4ebfb60ecb94c340f2d
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-20 14:05:09 +0530

    sb-security-jpa: Working with hard coded UserDetails

--- sb-security-jpa/src/main/java/poc/springboot/config/UserDetailsImpl.java
+++ sb-security-jpa/src/main/java/poc/springboot/config/UserDetailsImpl.java
@@ -1,52 +1,56 @@
 package poc.springboot.config;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserDetailsImpl implements UserDetails {
 
+	private String username;
+
+	public UserDetailsImpl() {
+	}
+
+	public UserDetailsImpl(String username) {
+		this.username = username;
+	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return "nopass";
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return username;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
--- sb-security-jpa/src/main/java/poc/springboot/config/UserDetailsServiceImpl.java
+++ sb-security-jpa/src/main/java/poc/springboot/config/UserDetailsServiceImpl.java
@@ -3,12 +3,14 @@ package poc.springboot.config;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		return new UserDetailsImpl(username);
 	}
 
 }
