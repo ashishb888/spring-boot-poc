0a74d8eb11a4d49597bbe8fd956fcc17c336c281
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-20 11:28:03 +0530

    sb-security-jdbc: Implemented Spring security with JDBC

---
+++ sb-security-jdbc/src/main/java/poc/springboot/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package poc.springboot.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN").antMatchers("/user").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/").permitAll().and().formLogin();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
---
+++ sb-security-jdbc/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into users values ('user', 'nopass', true);
+
+insert into users values ('admin', 'nopass', true);
+
+insert into authorities values ('user', 'ROLE_USER');
+
+insert into authorities values ('admin', 'ROLE_ADMIN');
