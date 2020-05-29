4055b71a975142ea3fc768996c240adaad1429af
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-29 10:47:11 +0530

    spring-security-facebook-login: Works well

---
+++ spring-security-facebook-login/src/test/java/poc/springboot/SpringSecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
