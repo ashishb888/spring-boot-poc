2b99c3dc9fb54eac250976d25d073624adc25408
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-29 10:27:57 +0530

    spring-security-github-login: Works well

---
+++ spring-security-github-login/src/main/java/poc/springboot/SpringSecurityGithubLoginApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGithubLoginApplication.class, args);
+	}
+
+}
---
+++ spring-security-github-login/src/test/java/poc/springboot/SpringSecurityGithubLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGithubLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
