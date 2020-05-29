5fd09a738508bc808d95f795a9dd6b85fe159092
ashishb888 <ashish.bhosle008@gmail.com>
2020-05-29 11:27:29 +0530

    spring-security-google-login: Works well

---
+++ spring-security-google-login/src/main/java/poc/springboot/SpringSecurityGoogleLoginApplication.java
@@ -0,0 +1,13 @@
+package poc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGoogleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGoogleLoginApplication.class, args);
+	}
+
+}
